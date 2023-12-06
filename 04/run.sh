9e59819b8b70de0e110bf682983e5c465daba03e
matoototo <matogudelj1205@gmail.com>
2023-12-06 09:53:10 +0100

    Restructure input/example/run.

---
+++ 04/run.sh
@@ -0,0 +1,11 @@
+# find local python file and run it
+
+echo "------------------"
+
+python3 ./*.py < example.txt
+
+echo "------------------"
+
+python3 ./*.py < input.txt
+
+echo "------------------"
