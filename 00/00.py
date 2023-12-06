249ca3ffa4ad6d2fbd007719c652213c4648ebba
matoototo <matogudelj1205@gmail.com>
2023-12-06 09:53:25 +0100

    Add template day.

---
+++ 00/00.py
@@ -0,0 +1,12 @@
+import sys
+
+def part_1(lines):
+    return -1
+
+def part_2(lines):
+    return -1
+
+if __name__ == '__main__':
+    lines = [line.rstrip('\n') for line in sys.stdin if line.rstrip('\n')]
+    print(part_1(lines))
+    print(part_2(lines))
